7c27e91461ad5321cec87f1339981ea33b0972f8
vibhorxpandey <vibhorpandey09@gmail.com>
2026-02-02 23:05:37 +0530

    Add files via upload

---
+++ dsaq2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+printf("enter a number: ");
+    // Input size of array
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    printf("enter the element");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos;
+    printf("enter a position: ");
+    // Input position to delete (1-based)
+    scanf("%d", &pos);
+
+    // Check if position is valid
+    if(pos < 1 || pos > n) {
+        printf("Invalid position");
+        return 0;
+    }
+
+    // Shift elements to the left
+    for(int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    // Print updated array (n-1 elements)
+    for(int i = 0; i < n - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
