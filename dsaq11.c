70d390e84dd993cf55d0cc508f7fab7300a505d4
vibhorxpandey <vibhorpandey09@gmail.com>
2026-02-11 10:46:02 +0530

    Add files via upload

---
+++ dsaq11.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int A[m][n], B[m][n], C[m][n];
+
+    printf("Enter elements of first matrix:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Addition
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    printf("Resultant matrix after addition:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
